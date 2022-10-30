0d974e92f1b8abede3f1ad06e210b42ee219c2d4
mtagizadee <magatagi0@gmail.com>
2022-10-30 20:01:38 +0400

    init

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className="w-full h-screen flex items-center justify-center font-bold text-xl">
+            Home Page
+        </div>
+    );
+}
+
+export default HomePage;
