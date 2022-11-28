8caec5f218ea069d989589c557b49d237801ef0c
CS-Front-Ender <shamxalquliyev@inbox.ru>
2022-11-28 17:04:00 +0400

    Forgot Password Page UI is ready. Back End has yet to be connected.

--- src/components/ui/GameTitle.tsx
+++ src/components/ui/GameTitle.tsx
@@ -1,7 +1,7 @@
 const GameTitle = () => {
     return (
         <h1
-            className='font-audiowide text-main-light text-[2.5rem] md:text-[3.7rem] md:max-w-[300px] mb-8'
+            className='font-audiowide text-main-light text-center md:text-left text-[2.5rem] md:text-[3.7rem] md:max-w-[300px] mb-8'
             style={{
                 mixBlendMode: 'normal',
                 textShadow: '0px 5px 20px rgba(189, 170, 147, 0.5)'
