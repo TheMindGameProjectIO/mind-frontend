4c549989cd680a13c8e70fd35a728bb27a3dfb0a
mtaghizada <magatagi0@gmail.com>
2022-12-22 22:37:00 +0400

    bg-image was added to the LobbiesLayout

--- src/components/layout/LobbiesLayout.tsx
+++ src/components/layout/LobbiesLayout.tsx
@@ -4,7 +4,7 @@ import Layout from "./Layout";
 const LobbiesLayout = () => {
   return (
     <Layout>
-      <div className="center-content full-screen bg-main-light">
+      <div className="center-content full-screen bg-about-game-background bg-no-repeat bg-contain bg-center">
         <div className="bg-dark-blue-200">
           <Outlet />
         </div>
