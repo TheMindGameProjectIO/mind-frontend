404069899d62f3474b077054ab490702304c7b91
mtaghizada <magatagi0@gmail.com>
2022-12-22 21:49:16 +0400

    prettier was added to the project

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,5 @@
 export const authRoutes = {
-    index: '/auth',
-    signup: '/auth/signup',
-    forgotPassword: '/auth/forgot-password',
-    google: '/auth/google', // FIXME: Delete them after connecting real google authentication
-    facebook: '/auth/facebook', // FIXME: Delete them after connecting real google authentication
-}
+  index: "/auth",
+  signup: "/auth/signup",
+  forgotPassword: "/auth/forgot-password",
+};
