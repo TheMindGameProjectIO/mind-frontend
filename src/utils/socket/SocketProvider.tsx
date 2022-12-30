3148a4c7c9f2d1cbc9a093753b40619c0194e88d
mtaghizada <magatagi0@gmail.com>
2022-12-30 16:57:27 +0400

    installed all requirements

--- src/utils/socket/SocketProvider.tsx
+++ src/utils/socket/SocketProvider.tsx
@@ -1,74 +1,71 @@
 import { useEffect, ReactNode, FC, createContext, useReducer, useContext, DependencyList } from "react";
-import socket from "./socket"
+import socket from "./socket";
 import { ServerToClientEvents } from "./types";
 
 /**
  * Socket Props (not expected to be changed)
  */
 interface Props {
-    /**
-     * consumers of the socket
-     */
-    children: ReactNode;
+  /**
+   * consumers of the socket
+   */
+  children: ReactNode;
 }
 
 /**
  * Socket Context
  */
 export interface ISocketContext {
-    /**
-     * socket connection status
-     */
-    isConnected: boolean;
+  /**
+   * socket connection status
+   */
+  isConnected: boolean;
 }
 
 const defaultContext: ISocketContext = {
-    isConnected: false,
-}
+  isConnected: false,
+};
 
 /**
  * Socket State
  */
-export interface IState extends Pick<ISocketContext, 'isConnected'> {
-
-}
+export interface IState extends Pick<ISocketContext, "isConnected"> {}
 
 const defaultState: IState = {
-    isConnected: false,
-}
+  isConnected: false,
+};
 
 export const SocketContext = createContext<ISocketContext>(defaultContext);
 
 const SocketProvider: FC<Props> = ({ children }) => {
-    const [{
-        // add your states here
-        isConnected
-    }, dispatch] = useReducer((state: IState, newState: Partial<IState>) => ({ ...state, ...newState }), defaultState);
+  const [
+    {
+      // add your states here
+      isConnected,
+    },
+    dispatch,
+  ] = useReducer((state: IState, newState: Partial<IState>) => ({ ...state, ...newState }), defaultState);
 
-    useEffect(() => {
-        socket.connection.on('connect', () => {
-            dispatch({ isConnected: true });
-        });
+  useEffect(() => {
+    socket.connection.on("connect", () => {
+      dispatch({ isConnected: true });
+    });
 
-        socket.connection.on('disconnect', () => {
-            dispatch({ isConnected: false });
-        });
+    socket.connection.on("disconnect", () => {
+      dispatch({ isConnected: false });
+    });
 
-        socket.connection.on("connect_error", (err) => {
-            console.log(`connect_error due to ${err.message}`);
-        })
+    socket.connection.on("connect_error", (err: any) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
 
-        return () => {
-            socket.connection.off('connect');
-            socket.connection.off('disconnect');
-        };
-    }, []);
+    return () => {
+      socket.connection.off("connect");
+      socket.connection.off("disconnect");
+    };
+  }, []);
 
-    return (
-        <SocketContext.Provider value={{ isConnected }}>
-            {children}
-        </SocketContext.Provider>
-    );
+  return <SocketContext.Provider value={{ isConnected }}>{children}</SocketContext.Provider>;
 };
 
 /**
@@ -76,8 +73,8 @@ const SocketProvider: FC<Props> = ({ children }) => {
  * @returns socket context states
  */
 export const useSocket = (): ISocketContext => {
-    const { isConnected } = useContext(SocketContext);
-    return { isConnected };
+  const { isConnected } = useContext(SocketContext);
+  return { isConnected };
 };
 
 /**
@@ -85,20 +82,24 @@ export const useSocket = (): ISocketContext => {
  * @param event event emmitted by the server
  * @param listener listener function
  * @param deps will re-subscribe to the event when any of the dependencies change
- * 
+ *
  * @example
  * useListenEvent('auth:verified:email', () => {
  *    console.log(user.email + ' is verified!');
  * }, [user.email]);
- * 
+ *
  */
-export function useListenEvent<Ev extends keyof ServerToClientEvents>(event: Ev, listener: ServerToClientEvents[Ev], deps?: DependencyList) {
-    useEffect(() => {
-        socket.connection.on(event, listener as any);
-        return () => {
-            socket.connection.off(event, listener as any);
-        };
-    }, deps);
+export function useListenEvent<Ev extends keyof ServerToClientEvents>(
+  event: Ev,
+  listener: ServerToClientEvents[Ev],
+  deps?: DependencyList
+) {
+  useEffect(() => {
+    socket.connection.on(event, listener as any);
+    return () => {
+      socket.connection.off(event, listener as any);
+    };
+  }, deps);
 }
 
 export default SocketProvider;
